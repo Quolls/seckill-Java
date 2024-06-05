5fcd546ba43aac0fd5908d512c4bb8f24abb7e06
iliiiiiili <yangquechen@gmail.com>
2024-06-05 18:58:08 +1000

    feat/add two tables

--- src/main/java/com/andy/seckill/SeckillApplication.java
+++ src/main/java/com/andy/seckill/SeckillApplication.java
@@ -1,9 +1,13 @@
 package com.andy.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.andy.seckill.db.mappers")
+@ComponentScan(basePackages = {"com.andy"})
 public class SeckillApplication {
 
     public static void main(String[] args) {
